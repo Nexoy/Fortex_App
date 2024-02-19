79fc289287cdf11f6fed0ee08fc9cba3da86139b
Nexoy <sebiksseba@gmail.com>
2024-02-19 20:23:54 +0100

    123

---
+++ app/src/main/java/com/example/fortex/Account_info_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.fortex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Account_info_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_info)
+    }
+}
---
+++ app/src/main/java/com/example/fortex/Game_Offert_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.fortex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game_Offert_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_offert)
+    }
+}
