c18e26387651b1dba933d643d382c72ab43ddaca
Nexoy <sebiksseba@gmail.com>
2024-02-22 00:04:54 +0100

    finish all layouts, new icons

--- app/src/main/java/com/example/fortex/Game_offer_Activity.kt
+++ app/src/main/java/com/example/fortex/Game_offer_Activity.kt
@@ -3,9 +3,9 @@ package com.example.fortex
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Game_Offert_Activity : AppCompatActivity() {
+class Game_offer_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_offert)
+        setContentView(R.layout.activity_game_offer)
     }
 }
